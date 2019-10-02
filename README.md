bdb49723f372b26cf3b35b0ceb0c5c1d47d468f7
goxler7 <GOXLER.7@GMAIL.COM>
2019-10-02 17:10:36 +0700

    init: start repair-design-project, setup webpack

---
+++ README.md
@@ -0,0 +1,7 @@
+# Repair Design Project
+
+### How to use it:
+1. Clone or download my repository 
+2. Install dependencies ```npm install``` 
+3. Start server at http://localhost:8081/ ```npm run dev``` 
+4. Build project for output files at dist folder ```npm run build```
---
+++ config/webpack.base.conf.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const fs = require('fs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
+
+const PAGES_DIR = PATHS.src
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'))
+
+module.exports = {
+  // BASE config
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    main: PATHS.src,
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }, 
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }, 
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `./config/postcss.config.js` } }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/style.[hash].css`,
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/${PATHS.assets}images`, to: `${PATHS.assets}images` },
+      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` }
+    ]),
+
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page}`
+    }))
+  ],
+}
